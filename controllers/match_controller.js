6b0675c13ca01cb0eb0e81cfc2e84f9cb40341d1
Burhanuddin <burhanuddinbohra72@gmail.com>
2021-12-19 16:09:56 +0530

    team details fetching from db

--- controllers/match_controller.js
+++ controllers/match_controller.js
@@ -1,32 +1,41 @@
-const request = require('request');
+const MatchLiveDetail = require('../models/match_live_details');
 
 module.exports.contest = function(req,res){
     const match_id = req.query.id;
     const homeTeamName = req.query.homeTeamName;
     const awayTeamName = req.query.awayTeamName;
-    const options = {
-        method: 'GET',
-        url: `https://cricket-live-data.p.rapidapi.com/match/${match_id}`,
-        headers: {
-        'x-rapidapi-host': 'cricket-live-data.p.rapidapi.com',
-        'x-rapidapi-key': process.env.API_KEY,
-        useQueryString: true
-        }
-    }; 
+    
     let matchDet = {
         "results": []
     };
-    let s;
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        let s = JSON.parse(body);
-        // console.log(s);
-        matchDet.results.push(s.results);
-        return res.render('contest_card', {
-        title: 'Contests',
-        match_details: matchDet,
-        homeTeamName: homeTeamName,
-        awayTeamName: awayTeamName
-        })
-    });
+    let lineOut = false;
+    MatchLiveDetail.findOne({matchId : match_id}, function(err , match){
+        if(err){
+            console.log('Error in finding match id in match details ');
+            return;
+            // return res.redirect('http://localhost:8000/users/sign-up');
+        }
+        else if(match){
+            lineOut=true;
+            let s = {
+                live_details : {
+                    teamsheets : {
+                        home : match.teamHomePlayers,
+                        away : match.teamAwayPlayers
+                    }
+                }
+            }
+            matchDet.results.push(s);
+        }else{
+            console.log('Live details are not out yet..');
+        }
+    })
+    
+    return res.render('contest_card', {
+    title: 'Contests',
+    match_details: matchDet,
+    homeTeamName: homeTeamName,
+    awayTeamName: awayTeamName,
+    lineupsOut : lineOut
+    })
 }
