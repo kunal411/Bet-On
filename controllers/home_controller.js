d9a77e2cb49c5e525db934f306c131743fa66b61
Burhanuddin <burhanuddinbohra72@gmail.com>
2021-12-20 19:59:17 +0530

    match status added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,53 +1,9 @@
 // const request = require('request');
 const { matchedData } = require('express-validator');
 const Matches = require('../models/match');
+const LiveMatches = require('../models/match_live_details');
 module.exports.home = function (req, res) {
-    
-    
-    // function pad2(n) {
-    //     return (n < 10 ? '0' : '') + n;
-    // }
-    // let obj = {
-    //     "results": []
-    // };
-    // var date = new Date();
-    // const date1 = new Date();
-    // var month = pad2(date.getMonth() + 1);//months (0-11)
-    // var day = pad2(date.getDate());//day (1-31)
-    // var year = date.getFullYear();
-    // var formattedDate = year + "-" + month + "-" + day;
-    // for (let i = 0; i < 5; i++) {
-    //     console.log(formattedDate);
-    //     const options = {
-    //         method: 'GET',
-    //         url: `https://cricket-live-data.p.rapidapi.com/fixtures-by-date/${formattedDate}`,
-    //         headers: {
-    //             'x-rapidapi-host': 'cricket-live-data.p.rapidapi.com',
-    //             'x-rapidapi-key': process.env.API_KEY,
-    //             useQueryString: true
-    //         }
-    //     };
-    //     request(options, function (error, response, body) {
-    //         if (error) throw new Error(error);
-    //         let s = JSON.parse(body);
-    //         // console.log(s);
-    //         for (mat of s.results) {
-    //             obj.results.push(mat);
-    //         }
-    //         if (i == 4) {
-    //             return res.render('match_card', {
-    //                 title: 'Home Page',
-    //                 matches: obj,
-    //                 currDate: date1
-    //             })
-    //         }
-    //     });
-    //     date = new Date(date.getTime() + (24 * 60 * 60 * 1000));
-    //     var month = pad2(date.getMonth() + 1);//months (0-11)
-    //     var day = pad2(date.getDate());//day (1-31)
-    //     var year = date.getFullYear();
-    //     formattedDate = year + "-" + month + "-" + day;
-    // }
+   
 
     if (!req.isAuthenticated()) {
         return res.redirect('http://localhost:8000/users/sign-in');
@@ -69,26 +25,42 @@ module.exports.home = function (req, res) {
         }
 
         for(let i = 0; i < matchList.length; i++){
-            let mat = {
-                match_title : matchList[i].matchTitle,
-                home: {
-                    name : matchList[i].teamHomeName,
-                    code: matchList[i].teamHomeCode.toUpperCase()
-                }, 
-                away: {
-                    name : matchList[i].teamAwayName,
-                    code: matchList[i].teamAwayCode.toUpperCase()
-                },
-                date: matchList[i].date,
-                id: matchList[i].matchId
-            }
-            obj.results.push(mat);
+            // console.log('iteration number : '+ i);
+            let liveStatus="";
+            LiveMatches.findOne({matchId : matchList[i].matchId}, function(err,match){
+                if(err){
+                    liveStatus = "Error, Cannot find the details of this match!!"
+                    // console.log('Error in finding live match in database!!');
+                }else if(match){
+                    liveStatus = "Line-ups are out";
+                }else{
+                    liveStatus = "Line-ups are not out yet!";
+                }
+                console.log(liveStatus);
+                let mat = {
+                    match_title : matchList[i].matchTitle,
+                    home: {
+                        name : matchList[i].teamHomeName,
+                        code: matchList[i].teamHomeCode.toUpperCase()
+                    }, 
+                    away: {
+                        name : matchList[i].teamAwayName,
+                        code: matchList[i].teamAwayCode.toUpperCase()
+                    },
+                    date: matchList[i].date,
+                    id: matchList[i].matchId,
+                    status : liveStatus
+                }
+                obj.results.push(mat);
+                if(i==matchList.length-1){
+                    return res.render('match_card', {
+                        title: 'Home Page',
+                        matches: obj,
+                        currDate: new Date()
+                    })
+                }
+            })
         }
-        return res.render('match_card', {
-            title: 'Home Page',
-            matches: obj,
-            currDate: new Date()
-        })
 
     })
 }
