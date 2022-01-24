8eb1c2028e9aaf7380031296fdf2f11df18fd633
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2022-01-24 22:32:33 +0530

    sd

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -5,12 +5,11 @@ class ChatEngine{
         this.userEmail = userEmail;
         this.chatRoom = chatRoom;
 
-        this.socket = io.connect(`https://domino-beton.herokuapp.com/match/contest/leaderboard?matchId=2554181&contestId=${chatRoom}`, { transports : ['websocket', 'polling'] });
+        this.socket = io.connect('https://domino-beton.herokuapp.com/match/contest/leaderboard/', { transports : ['websocket', 'polling'] });
 
         if (this.userEmail){
             this.connectionHandler();
         }
-
     }
 
 
