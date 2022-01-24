c42c162041db73e9f56f2abbe5d196b05a4c0888
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2022-01-24 19:21:12 +0530

    deploy

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -5,7 +5,7 @@ class ChatEngine{
         this.userEmail = userEmail;
         this.chatRoom = chatRoom;
 
-        this.socket = io.connect('wss://domino-beton.herokuapp.com', { transports : ['websocket'] });
+        this.socket = io.connect('wss://domino-beton.herokuapp.com:5100', { transports : ['websocket'] });
 
         if (this.userEmail){
             this.connectionHandler();
