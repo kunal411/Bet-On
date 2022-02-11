2b29210969ca508db0dbc019aaab634465f7db91
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2022-02-11 14:30:28 +0530

    Made changes in css

--- config/environment.js
+++ config/environment.js
@@ -15,9 +15,9 @@ const development = {
     name : 'development',
     asset_path : './public/assets',
     session_cookie_key : 'betonTime',
-    google_client_iD: "359903220192-qfi6cfmvl3ilq5fk1a210rnuebm1r6p1.apps.googleusercontent.com",
-    google_client_secret: "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",
-    google_callbackURL: "http://localhost:8000/users/auth/google/callback",
+    google_client_iD: "1079664389518-m4uaid1nv7rhhvm1cp6ins5o2nivag6g.apps.googleusercontent.com",
+    google_client_secret: "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",
+    google_callbackURL: "https://domino-beton.herokuapp.com/users/auth/google/callback",
     morgan : {
         mode: 'dev',
         options : {stream: accessLogStream}
